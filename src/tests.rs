e54197f763437abfdbdc3b735b86d20720daf24d
BurritoBandit28 <yansalmon@gmail.com>
2025-01-18 17:52:51 +0000

    Improved tests for normalising and multiplying vector tuples

--- src/tests.rs
+++ src/tests.rs
@@ -12,19 +12,49 @@ mod tests {
 
     #[test]
     fn mul_vec_test() {
+        // create a vector tuple
         let mut vector = (3.5, 6.0);
+        // multiply by 4
         mul_vec(&mut vector, 4.0);
-        assert_eq!(vector, (14.0, 24.0))
+        assert_eq!(vector, (14.0, 24.0));
+
+        // multiply by -1
+        mul_vec(&mut vector, -1.0);
+        assert_eq!(vector, (-14.0, -24.0));
+
+        // multiply by 0
+        mul_vec(&mut vector, 0.0);
+        assert_eq!(vector, (0.0, 0.0));
+
     }
 
     #[test]
     fn normalise_vec_test() {
-        let vector = (3.0, 4.0);
+        // create vector tuple
+        let vector = (3.0, 4.0); // Pythagorean Triple 3^2 + 4^2 = 5^2
+
+        // get magnitude
         let mag = f32::sqrt((vector.0 * vector.0) + (vector.1 * vector.1));
+
+        // equals 5
         assert_eq!(mag, 5.0);
+
+        // normalise vector
         let normalised = normalise_vec(vector);
+
+        // the magnitude should now be one, as it is a unit vector
         let mag_normalised = f32::sqrt((normalised.0 * normalised.0) + (normalised.1 * normalised.1));
-        assert_eq!(mag_normalised, 1.0)
+        assert_eq!(mag_normalised, 1.0);
+
+        // test for safely handling when magnitude is 0
+        let vector = (0.0, 0.0);
+
+        // when normalising, the x and y is divided by the magnitude
+        // when the magnitude is 0, this isn't possible as x/0 is undefined
+        let normalised = normalise_vec(vector);
+
+        // instead of dividing, just return (0,0)
+        assert_eq!(normalised, (0.0, 0.0));
     }
 
     #[test]
