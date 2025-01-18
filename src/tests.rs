8a33afd77669d08fdd7d33c48e5f0bf4b7c6543f
BurritoBandit28 <yansalmon@gmail.com>
2025-01-18 12:11:18 +0000

    Added a test for world space screen space conversion

--- src/tests.rs
+++ src/tests.rs
@@ -3,9 +3,12 @@
 
 #[cfg(test)]
 mod tests {
-    use num::integer::sqrt;
+    use std::sync::Mutex;
+    use crate::entities::dummy::DummyEntity;
+    use crate::entity::Entity;
+    use crate::game::Game;
+    use crate::render::AssetData;
     use crate::utils::{mul_vec, normalise_vec};
-    use super::*;
 
     #[test]
     fn mul_vec_test() {
@@ -23,4 +26,30 @@ mod tests {
         let mag_normalised = f32::sqrt((normalised.0 * normalised.0) + (normalised.1 * normalised.1));
         assert_eq!(mag_normalised, 1.0)
     }
+
+    #[test]
+    fn world_space_screen_space_test() {
+        // create a game instance
+        let mut game = Game::initiate();
+        // create an entity
+        let mut dummy = DummyEntity::create(&mut game, AssetData::empty()); // represents entity
+
+        // set its coords
+        dummy.set_coords((-80f32, 0f32));
+
+        // create player coordinates
+        let player_coords = (0f32, 0f32);
+
+        // get relative screen coordinates
+        let mut out = dummy.screen(player_coords);
+
+        assert_eq!(out, (80, 90));
+
+        // again for off-screen coordinates
+        dummy.set_coords((235f32, -103f32));
+
+        out = dummy.screen(player_coords);
+        assert_eq!(out, (395, -13))
+    }
+
 }
